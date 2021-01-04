ecd332514b48db2e4637acfab4837146dc8381da
Darren Gibbard <dalgibbard@gmail.com>
2021-01-04 22:49:25 +0000

    Condense a bit

--- weatherhistory.py
+++ weatherhistory.py
@@ -15,26 +15,20 @@ Hourly.cache_dir = "/tmp/weather_cache"
 start, end = (datetime(2020, 1, 1), datetime(2020, 12, 31))
 # Geographic co-ordinates (Basildon, UK)
 lat, lon = (51.5761, 0.4887)
+# Time of day
+timeofday = 12
 
 # Get nearest station to location
 stations = Stations()
 stations = stations.nearby(lat, lon)
-stations = stations.inventory('daily', (start, end))
-station = stations.fetch(1)
+station = stations.inventory('daily', (start, end)).fetch(1)
 
-df = Hourly(station, start, end)
-df = df.fetch()  # returns a pandas dataframe
+df = Hourly(station, start, end).fetch() # returns a pandas dataframe
 
-# Add columns for hour / minute in the dataframe, from the 'time' index.
-df["hour"] = df.index.hour
-df["minute"] = df.index.minute
-
-# Create a new dataframe containing only values for midday
-newdf = df[(df.hour == 12) & (df.minute == 0)]
-# Create a new dataframe of midday timing, containing only the temperature, rounded, as integers.
-temps = newdf["temp"].round(decimals=0).astype(int)
+# Create a new dataframe containing only temperature values for midday, rounded to nearest int
+newdf = df["temp"][(df.index.hour == timeofday) & (df.index.minute == 0)].round(decimals=0).astype(int)
 # Print it for review
-print(temps)
+print(newdf)
 
 # Output to Excel sheet
-temps.to_excel("2020-weather.xlsx", engine='xlsxwriter')
+newdf.to_excel("2020-weather.xlsx", engine='xlsxwriter')
